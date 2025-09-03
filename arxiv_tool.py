4aafc313be7a6a0e16234cda349be99439787473
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-09-03 23:58:29 +0500

    url implemented in arxiv_tool.py

--- arxiv_tool.py
+++ arxiv_tool.py
@@ -2,20 +2,35 @@
 import requests
 
 
-def search_arxiv_papers(topics: str, max_results: int = 5) -> dict:
-    query = "Prompt Engineering"
+def search_arxiv_papers(topic: str, max_results: int = 5) -> dict:
+    query = "+".join(topic.lower().split())
+    for char in list('()" '):
+        if char in query:
+            print(f"Invalid character '{char}' in query: {query}")
+            raise ValueError(
+                f"Cannot have character: '{char}' in query: {query}")
 
     url = (
         "http://export.arxiv.org/api/query"
         f"?search_query=all:{query}"
         f"&max_results={max_results}"
-        # "&sortBy=submittedtDate"
+        "&sortBy=submittedDate"
         "&sortOrder=descending"
     )
 
+    print(f"Making request to arxiv API: {url}")
     resp = requests.get(url)
-    print("response: ", resp.status_code)
-    print("response: ", resp.text)
+
+    if not resp.ok:
+        print(f"ArXiv Api request failed: {resp.status_code} - {resp.text}")
+        raise ValueError(f"Bad response from arXiv Api: {resp}\n{resp.text}")
+
+    # data = parse_arxiv_xml(resp.text)
+    # return data
+    return resp.text
+
+
+print(search_arxiv_papers(topic="Prompt engineering", max_results=2))
 
 # step2: parse XML
 # step3: convert the functionality into a tool
