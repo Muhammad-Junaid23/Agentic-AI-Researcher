3f2db458d79785d05627df6208eaed705d9bcbaf
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-09-03 23:36:08 +0500

    arxiv_tool

--- arxiv_tool.py
+++ arxiv_tool.py
@@ -1,20 +1,21 @@
 # step1: Access arXiv using URL
 import requests
 
-query = "Prompt Engineering"
-max_results = 1
 
-url = (
-    "http://export.arxiv.org/api/query"
-    f"?search_query=all:{query}"
-    f"&max_results={max_results}"
-    # "&sortBy=submittedtDate"
-    "&sortOrder=descending"
-)
+def search_arxiv_papers(topics: str, max_results: int = 5) -> dict:
+    query = "Prompt Engineering"
 
-resp = requests.get(url)
-print("response: ", resp.status_code)
-print("response: ", resp.text)
+    url = (
+        "http://export.arxiv.org/api/query"
+        f"?search_query=all:{query}"
+        f"&max_results={max_results}"
+        # "&sortBy=submittedtDate"
+        "&sortOrder=descending"
+    )
+
+    resp = requests.get(url)
+    print("response: ", resp.status_code)
+    print("response: ", resp.text)
 
 # step2: parse XML
 # step3: convert the functionality into a tool
